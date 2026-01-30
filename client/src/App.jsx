c6808c3ba22275a8f36b8e1f1b72d70942d639a7
kingofdead6 <ny_kahlouche@esi.dz>
2026-01-30 16:01:32 +0100

    Added new features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,10 +38,7 @@ function App() {
               <Route path="/admin/products" element={<AdminProducts />} />
               <Route path="/admin/categories" element={<AdminCategories />} />
               <Route path="/admin/delivery-areas" element={<AdminDeliveryAreas />} />
-              <Route path="/admin/users" element={<AdminUsers />} />
-              <Route path="/admin/orders" element={<AdminOrders />} />
-            </Route>
-
+              <Route path="/admin/use
       </Routes>
       <Footer />
     </Router>
